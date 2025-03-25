452965db9b0e842c1ac74598af495bd10f17ebec
Wernod <xrustik08+GitHub@gmail.com>
2025-03-25 20:43:27 +0200

    created logo and name

--- frontend/src/components/registerForm.jsx
+++ frontend/src/components/registerForm.jsx
@@ -20,7 +20,7 @@ function registerForm () {
         }
 
         try {
-            const response = await axios.post("http://localhost:5200/register", {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/register`, {
                 email,
                 password,
             });
